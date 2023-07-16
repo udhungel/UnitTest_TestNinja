41fa331fd1b31b88b5b8ae5a06d4c528d4f1690a
udhungel <udeepdhungel@gmail.com>
2023-07-16 15:35:54 +0200

    Refactoring Code base

--- TestNinja/Fundamentals/Reservation.cs
+++ TestNinja/Fundamentals/Reservation.cs
@@ -6,17 +6,8 @@
 
         public bool CanBeCancelledBy(User user)
         {
-            //return (user.IsAdmin || MadeBy == user);
-
-            if (user.IsAdmin)
-            {
-                return true;
-
-            }
-            if (MadeBy == user) { return true; }
-            return false;   
-        }
-        
+            return (user.IsAdmin || MadeBy == user);                
+        }        
     }
 
     public class User
