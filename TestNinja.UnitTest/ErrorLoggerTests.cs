53a1a36b41970611225a4efa3e178f1c6fe0698b
udhungel <udeepdhungel@gmail.com>
2023-07-16 21:23:17 +0200

    Unit Test  -  Testing Void Methods

---
+++ TestNinja.UnitTest/ErrorLoggerTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTest
+{
+    [TestFixture]
+    public class ErrorLoggerTests
+    {
+        [Test]
+        public void Log_WhenCalled_SetLastErrorProperty()
+        {
+            var logger = new ErrorLogger();
+            logger.Log("a");
+
+            Assert.That(logger.LastError, Is.EqualTo("a"));
+
+        }
+    }
+}
