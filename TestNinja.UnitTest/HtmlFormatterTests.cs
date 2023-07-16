dc62570ed5924617c0558492ede28dc9f814339f
udhungel <udeepdhungel@gmail.com>
2023-07-16 19:27:29 +0200

    Unit Test - Strings

---
+++ TestNinja.UnitTest/HtmlFormatterTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTest
+{
+    [TestFixture]
+    public class HtmlFormatterTests
+    {
+        [Test]
+        public void FormatAsBold_WhenCalled_ShouldEncloseTheStringWithStrongElement()
+        {
+            var formatter = new HtmlFormatter();
+
+            var result = formatter.FormatAsBold("abc");
+
+            //Specific Assertion
+            Assert.That(result, Is.EqualTo("<strong>abc</strong>").IgnoreCase);
+
+            ////More general
+            //Assert.That(result, Does.StartWith("<strong>").IgnoreCase);
+            //Assert.That(result, Does.EndWith("</strong>"));
+            //Assert.That(result, Does.Contain("abc"));
+
+        }
+    }
+}
