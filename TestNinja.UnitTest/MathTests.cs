70574a9df3073c67729c76f059c11b4f3d4c5665
udhungel <udeepdhungel@gmail.com>
2023-07-16 17:25:39 +0200

    Unit Test for Math.cs Added logic for all the scenarios

---
+++ TestNinja.UnitTest/MathTests.cs
@@ -0,0 +1,53 @@
+﻿using NUnit.Framework;
+using System;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTest
+{
+    [TestFixture]
+    public class MathTests
+    {
+        [Test]
+        public void Add_WhenCalled_SumofArguments()
+        {
+            //Arrange
+            //Add
+            //Assert
+            Fundamentals.Math mathTest = new Fundamentals.Math();
+
+            var result = mathTest.Add(1, 2);
+
+            Assert.AreEqual(3, result);
+        }
+        [Test]
+        public void Max_FirstArgumentIsGreater_ReturnTheFirstArgument()
+        {
+            Fundamentals.Math mathTest = new Fundamentals.Math();
+
+            var result = mathTest.Max(2, 1);
+
+            Assert.AreEqual(2, result);
+        }
+
+        [Test]
+        public void Max_SecondArgumentIsGreater_ReturnTheSecondArgument()
+        {
+            Fundamentals.Math mathTest = new Fundamentals.Math();
+
+            var result = mathTest.Max(1, 2);
+
+            Assert.AreEqual(2, result);
+
+        }
+
+        [Test]
+        public void Max_ArgumentAreEqual_ReturnTheSameArgument()
+        {
+            Fundamentals.Math mathTest = new Fundamentals.Math();
+
+            var result = mathTest.Max(1, 1);
+
+            Assert.AreEqual(1, result);
+        }
+    }
+}
