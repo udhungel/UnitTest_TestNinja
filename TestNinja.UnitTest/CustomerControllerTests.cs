9b825506bef08076ae6d93163e469a401179a4ea
udhungel <udeepdhungel@gmail.com>
2023-07-16 20:54:51 +0200

    Unit Test - Testing Return type methods

---
+++ TestNinja.UnitTest/CustomerControllerTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTest
+{
+    [TestFixture]
+    public class CustomerControllerTests
+    {
+        [Test]
+        public void GetCustomer_IdIsZero_ReturnNotFound()
+        {
+            var controller = new CustomerController();
+            var result = controller.GetCustomer(0);
+            Assert.That(result, Is.TypeOf<NotFound>());
+            // other way 
+            //Assert.That(result, Is.InstanceOf<NotFound>());
+        }
+
+        [Test]
+        public void GetCustomer_IdNotZero_ReturnOk()
+        {
+            var controller = new CustomerController();
+            var result = controller.GetCustomer(1);
+            Assert.That(result, Is.TypeOf<Ok>());
+        }
+    }
+}
