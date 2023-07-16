2496c520b8c3a5827bbb3232c17a20baa0ae7260
udhungel <udeepdhungel@gmail.com>
2023-07-16 21:38:14 +0200

    Testing Method that Raise Event

--- TestNinja.UnitTest/ErrorLoggerTests.cs
+++ TestNinja.UnitTest/ErrorLoggerTests.cs
@@ -27,10 +27,23 @@ namespace TestNinja.UnitTest
         [TestCase(" ")]
         public void Log_InvalidError_ThrowArgumentNullException(string error)
         {
-            var logger = new ErrorLogger(); 
+            var logger = new ErrorLogger();
             //logger.Log(error); this will throw error need a delegate 
 
-            Assert.That(()=> logger.Log(error), Throws.ArgumentNullException);
+            Assert.That(() => logger.Log(error), Throws.ArgumentNullException);
         }
+
+        [Test]
+        public void Log_VAlidError_RaiseErorrLoggedEvent()
+        {
+            var logger = new ErrorLogger();
+            var id = Guid.Empty;
+            //check event is raised before the method fires if so id should have some value
+            logger.ErrorLogged += (sender, args) => { id = args;};  
+            logger.Log("a");
+            Assert.That(id, Is.Not.EqualTo(Guid.Empty));
+
+        }
+
     }
 }
